910bb29cb04757afac91d2958a7ab47a26f4b41e
koraish3438 <koraishmdkajamall38@gmail.com>
2025-10-25 12:44:27 +0600

    Second commit

--- app/src/main/java/com/example/messenger/MyAdapter.kt
+++ app/src/main/java/com/example/messenger/MyAdapter.kt
@@ -1,4 +1,24 @@
-package com.example.messenger;
+package com.example.messenger
 
-public class MyAdapter: RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(private var data: List<String>, private val layout: Int, private val listener: AdapterView.OnItemClickListener): RecyclerView.Adapter<MyAdapter.MyViewAdapter> {
+    class MyViewAdapter(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewAdapter {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewAdapter, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
